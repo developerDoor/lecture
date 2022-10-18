27884d1ff20b3586f9ab970315b9396d648d3d2e
jhmoon <dothebestmoon1016@gmail.com>
2022-10-18 17:56:38 +0900

    로그인 강의 수강

--- login-lecture/app/app.js
+++ login-lecture/app/app.js
@@ -2,10 +2,20 @@
 
 // 모듈
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const fs = require('fs');
+
 const app = express();
+dotenv.config();
+const accessLogStream = require('./src/config/log');
 
 // 라우팅
 const home = require("./src/routes/home")
+
+const logger = require('./src/config/logger');
+logger.log('info', 'hello 구독자님들.');
+
 
 // 앱 세팅
 app.set("views", "./src/views");
@@ -13,6 +23,8 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(morgan('dev'));
+app.use(morgan('common', { stream : accessLogStream }), );
 app.use("/", home) // use -> 미들웨어를 등록해주는 메서드.
 
 module.exports = app;
